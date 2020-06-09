2a9cfc8a157b1b202e6fa158490f5e9d8813c0c1
Black3 <youngerblack@gmail.com>
2020-06-08 21:31:13 -0500

    first commit for Intellij

---
+++ src/Fibonacci.kt
@@ -0,0 +1,19 @@
+fun main() {
+
+    println("Hello, world!")
+
+    val numeroFibonacci =10
+
+    for ( data in 0..numeroFibonacci){
+
+
+        val contador = data
+        val fibonacci = contador + data
+
+        print ("$contador, ")
+        //print ("$fibonacci, ")
+
+    }
+
+
+}
